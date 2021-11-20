091206c89db2d797bcbeb7bd5c887c376950a451
Sadi <sadigulbey@hotmail.com>
2021-11-20 16:02:25 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/sucial_cs310_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sucial_cs310_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:sucial_cs310_project/routes/walkthrough.dart';
+import 'package:sucial_cs310_project/routes/login.dart';
+import 'package:sucial_cs310_project/routes/signup.dart';
+import 'package:sucial_cs310_project/routes/welcome.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/walkthrough',
+    routes: {
+        '/walkthrough': (context) => WalkThrough(),
+        '/login': (context) => Login(),
+        '/signup': (context) => Signup(),
+        '/welcome': (context) => Welcome(),
+      },
+
+  ));
+}
+
