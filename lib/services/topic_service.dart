7675b79fd7758c4d9fa941c8cd1bc16284c4b5e8
sinanyilmaz1 <sinanyilmaz@sabanciuniv.edu>
2022-01-05 19:42:37 +0300

    started to implement topic

---
+++ lib/services/topic_service.dart
@@ -0,0 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:sucial_cs310_project/model/post.dart';
+
+
+class TopicService{
+  final CollectionReference topics = FirebaseFirestore.instance.collection('Topic');
+
+  addTopicto()async{
+
+  }
+
+
+
+}
